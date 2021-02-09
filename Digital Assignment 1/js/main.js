5913dc566ad7dc9c0cc90ac08f120386cd1a0f6b
BerkleyUmbaugh <61843640+BerkleyUmbaugh@users.noreply.github.com>
2021-02-09 16:04:44 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -1,59 +1,87 @@
 import "./phaser.js";
 
-var config = {
+// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
+// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
+// `phaser-example` to `game`, which is the id of the HTML element where we
+// want the game to go.
+// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
+// You will need to change the paths you pass to `this.load.image()` or any other
+// loading functions to reflect where you are putting the assets.
+// All loading functions will typically all be found inside `preload()`.
+
+// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+
+class MyScene extends Phaser.Scene {
+    
+    constructor() {
+        super();
+        
+        this.bouncy = null;
+    }
+    
+    preload() {
+        // Load an image and call it 'logo'.
+        this.load.image( 'logo', 'assets/phaser.png' );
+		this.load.image( 'ball', 'assets/Basketball.jpeg' );
+    }
+    
+    create() {
+        
+	var ball = this.add.sprite(200, 300, 'ball').setInteractive();
+	
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.physics.arcade.gravity.y = 100;
+	    
+	this.input.setDraggable(ball);
+	this.input.dragDistanceThreshold = 1;
+		
+	this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+		
+		// Create a sprite at the center of the screen using the 'logo' image.
+        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
+        
+        // Make it bounce off of the world bounds.
+        this.bouncy.body.collideWorldBounds = true;
+        
+        // Make the camera shake when clicking/tapping on it.
+        this.bouncy.setInteractive();
+        this.bouncy.on( 'pointerdown', function( pointer ) {
+            this.scene.cameras.main.shake(500);
+            });
+        
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        let text = this.add.text( this.cameras.main.centerX, 15, "Hello Berkley", style );
+        text.setOrigin( 0.5, 0.0 );
+		
+	game.physics.enable([ball], Phaser.Physics.ARCADE);
+	ball.body.collideWorldBounds = true;
+	ball.body.bounce.y = 0.8;
+	
+	}	
+	
+    
+    update() {
+        // Accelerate the 'logo' sprite towards the cursor,
+        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
+    }
+}
+
+const game = new Phaser.Game({
     type: Phaser.AUTO,
+    parent: 'game',
     width: 800,
     height: 600,
-    backgroundColor: '#010101',
-    parent: 'phaser-example',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 },
-            debug: true
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var container;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-   this.load.image('mushroom', 'assets/Basketball.jpeg');
-}
-
-function create ()
-{
-    var image1 = this.add.image(0, -30, 'mushroom');
-    var image2 = this.add.image(-40, 30, 'mushroom');
-    var image3 = this.add.image(40, 30, 'mushroom');
-
-    container = this.add.container(400, 200, [ image1, image2, image3 ]);
-
-    //  A Container has a default size of 0x0, so we need to give it a size before enabling a physics
-    //  body or it'll be given the default body size of 64x64.
-    container.setSize(128, 64);
-
-    this.physics.world.enable(container);
-
-    container.body.setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true);
-}
-
-function update ()
-{
-    if (container.body.velocity.x < 0)
-    {
-        container.rotation -= 0.02;
-    }
-    else
-    {
-        container.rotation += 0.02;
-    }
-}
+    scene: MyScene,
+    physics: { default: 'arcade' },
+    });
